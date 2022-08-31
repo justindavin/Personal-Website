db748ce9b39685d1695c22f060008e2327ca400c
justindavin <justindavinlee@gmail.com>
2022-08-31 03:53:43 -0400

    cv

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -59,7 +59,7 @@ const Sidebar = () => {
                     <a
                     target="_blank"
                     rel='noreferrer'
-                    href="https://drive.google.com/file/d/1dgN0flqUgPJkDfFTNNgmOZisUULHAWkU/view?usp=sharing">
+                    href="https://drive.google.com/file/d/1hD9tCFVRrmnb7EO9LpKyYxzYhatn4WH-/view?usp=sharing">
                         <FontAwesomeIcon icon={faFilePdf} color="#4d4d4e" />
                     </a>
                 </li>
